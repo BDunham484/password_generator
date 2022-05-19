5eecbbd6ea05f6235af01137d52176dbb4f663a8
ThereWolf <bdunham484@gmail.com>
2022-05-19 18:32:27 -0500

    troubleshooting

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,8 @@ var passwordArr = [];
 
 // create function to ask user for password generatePassword
 var generatePassword = function() {
-  var passwordText = document.querySelector("#password");
-  passwordText.value = "";
+  
+  mixedChars = [];
   //create variable with prompt to determine password length with confirmation message
   var passwordLength = window.prompt("How long would you like your password to be?  Please enter a number between 8 and 128.")
 
@@ -74,8 +74,10 @@ var generatePassword = function() {
     window.alert("You must pick at least 1 type of character! Please try again.")
     return generatePassword();
   }
+  // debugger;
   //clears the password array
   var passwordArr = [];
+  
   // creates and ranomizes password  
   for (var i = 0; i < passwordLength; i++) {
     arrLength = mixedChars.length;
@@ -93,6 +95,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // debugger;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
